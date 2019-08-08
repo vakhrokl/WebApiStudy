a189d20e6836112f927265af229b84f04a022bae
Кирилл Вахромеев <kirill.vahromeev@spellabs.com>
2019-08-08 17:41:35 +0300

    Добавлена страница с аякс подержкой веб сервиса

--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,8 @@ namespace TodoApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
